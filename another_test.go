bb8ffecffff6075cf578844b5347bd2a5c971032
Aiden Wang <aidenwang@google.com>
2022-05-27 22:02:45 +0000

    test2

--- another_test.go
+++ another_test.go
@@ -37,3 +37,37 @@ func FuzzReverse2(foo_bar_fooBar *testing.F) {
 		}
 	})
 }
+
+func FuzzReverse2_12312_foooooo(foo_bar_fooBar *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+	for _, tc := range testcases {
+		foo_bar_fooBar.Add(tc) // Use f.Add to provide a seed corpus
+	}
+	foo_bar_fooBar.Fuzz(func(t *testing.T, orig string) {
+		rev := Reverse2(orig)
+		doubleRev := Reverse2(rev)
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
+}
+
+func FuzzReverse2_barr_BarBBAARbaR___aaaa(foo_bar_fooBar *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+	for _, tc := range testcases {
+		foo_bar_fooBar.Add(tc) // Use f.Add to provide a seed corpus
+	}
+	foo_bar_fooBar.Fuzz(func(t *testing.T, orig string) {
+		rev := Reverse2(orig)
+		doubleRev := Reverse2(rev)
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
+}
